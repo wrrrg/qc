f3245e3c5334e903c44540ec21219f12f950aa13
wrrrg <bill.gryta@gmail.com>
2018-08-07 18:33:18 -0500

    header transitions and grid

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,16 @@
-jQuery(function () {
-    jQuery("#P1").YTPlayer();
+$(document).ready(function() {});
+
+$(window).on("scroll", function() {
+  var height = $(window).height();
+  var double = height * 2;
+  if ($(window).scrollTop() >= $(window).height()) {
+    $(".hem").addClass("header-dark");
+  } else {
+    //remove the background property so it comes transparent again (defined in your css)
+    $(".hem").removeClass("header-dark");
+  }
+
+  if ($(window).scrollTop() >= double) {
+    $(".hem").removeClass("header-dark");
+  }
 });
