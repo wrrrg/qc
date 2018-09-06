ccd47f7f310f94aab0c5aa0c3d41c8ec40ce0c18
wrrrg <bill.gryta@gmail.com>
2018-09-06 12:30:57 -0500

    rrh background / player

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {});
+// $(document).ready(function() {});
 
-$(window).on("scroll", function() {
-  var height = $(window).height();
-  var double = height * 2;
-  if ($(window).scrollTop() >= $(window).height()) {
-    $(".hem").addClass("header-dark");
-  } else {
-    //remove the background property so it comes transparent again (defined in your css)
-    $(".hem").removeClass("header-dark");
-  }
+// $(window).on("scroll", function() {
+//   var height = $(window).height();
+//   var double = height * 2;
+//   if ($(window).scrollTop() >= $(window).height()) {
+//     $(".hem").addClass("header-dark");
+//   } else {
+//     //remove the background property so it comes transparent again (defined in your css)
+//     $(".hem").removeClass("header-dark");
+//   }
 
-  if ($(window).scrollTop() >= double) {
-    $(".hem").removeClass("header-dark");
-  }
-});
+//   if ($(window).scrollTop() >= double) {
+//     $(".hem").removeClass("header-dark");
+//   }
+// });
